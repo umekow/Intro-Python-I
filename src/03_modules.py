0933b9e5ce5fe65ed783c9d0528253cfe815140c
Restyled.io <commits@restyled.io>
2020-02-10 22:41:22 +0000

    Restyled by autopep8

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,12 +5,13 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-for x in sys.argv: 
+for x in sys.argv:
     print(x)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
@@ -22,7 +23,6 @@ print(sys.platform)
 print(sys.version)
 
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
